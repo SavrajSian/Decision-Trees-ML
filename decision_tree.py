9259483c5b7c3cfb5b091aecd1777ee051115e42
Jamie Todd <jmt20@ic.ac.uk>
2022-10-19 12:17:43 +0100

    k-fold cross validation

--- decision_tree.py
+++ decision_tree.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.random import default_rng
 
 
 class Node:
@@ -34,9 +35,8 @@ def find_split(data):
     return best_attribute, best_value, best_l_split, best_r_split
 
 
-def info_gain(dataset, left, right):
-    # dataset is whole dataset, left is left part of where splitting, same for right
-    h_all = entropy(dataset)
+def info_gain(h_all, left, right):
+    # left is the left part of split, right is the right part
     left_size = left.size
     right_size = right.size
     total_size = left_size + right_size
@@ -55,7 +55,7 @@ def entropy(dataset):
     return hs.sum()
 
 
-def decision_tree_learning(training_dataset, depth) -> (Node, int):
+def decision_tree_learning(training_dataset, depth):
     y = training_dataset[:, -1]
 
     if np.min(y) == np.max(y):  # All samples have same categorical value
@@ -71,12 +71,82 @@ def decision_tree_learning(training_dataset, depth) -> (Node, int):
 
 def fit(model, sample):
     if model.leaf:
-        return model.value
-    else:
-        if sample[model.attribute] < model.value:
-            return fit(model.left, sample)
-        else:
-            return fit(model.right, sample)
+        return int(model.value)
+    if sample[model.attribute] < model.value:
+        return fit(model.left, sample)
+    return fit(model.right, sample)
+
+
+def split_dataset(x, test_proportion, random_generator=default_rng()):
+    size = len(x)
+    shuffled_indices = random_generator.permutation(size)
+    n_train = round(size * (1 - test_proportion))
+    x_train = x[shuffled_indices[:n_train]]
+    x_test = x[shuffled_indices[n_train:]]
+    return x_train, x_test
+
+
+def evaluate(model, test_dataset):
+    success = 0
+    for sample in test_dataset:
+        result = fit(model, sample[:-1])
+        if result == sample[-1]:
+            success += 1
+    accuracy = 100 * (success / test_dataset.shape[0])
+    print("Accuracy: " + str(accuracy))
+
+
+def update_confusion_matrix(confusion_matrix, model, test):
+    for sample in test:
+        x, y = sample[:-1], int(sample[-1])
+        result = fit(model, x)
+        # TODO: Ensure this is indexed the correct way (matt is unsure)
+        # TODO: x-axis should be determined labels, y-axis actual labels
+        confusion_matrix[(result - 1, y - 1)] += 1
+    return confusion_matrix
+
+
+def draw_tree(model):
+    # TODO: Draw a tree w/ matplotlib
+    pass
+
+
+def calc_recall_precision(confusion_matrix):
+    # Calculate recall and precision rates per class
+    pass
+
+
+def prune(model):
+    #  TODO: evaluates and prunes a given model (probably recursive)
+    pass
+
+
+def k_fold_cross_validation(dataset, k):
+    # Split data k equal ways
+    # Repeat k times:
+    #   Take a subset of data to test
+    #   Train model using other k-1 subsets
+    #   Evaluate
+    shuffled_indices = default_rng().permutation(len(dataset))
+    data_buckets = []
+    interval = dataset.shape[0] // k
+
+    for i in range(0, dataset.shape[0] + 1, interval):
+        data_buckets.append(dataset[shuffled_indices[i:i + interval]])
+
+    confusion_matrix = np.zeros(shape=(4, 4), dtype=int)
+
+    for i in range(k):
+        test = data_buckets.pop(0)  # Remove from front
+        model, _ = decision_tree_learning(np.concatenate(data_buckets), 0)  # Train on remaining
+        data_buckets.append(test)  # Add back to end
+        confusion_matrix = update_confusion_matrix(confusion_matrix, model, test)
+
+    # TODO: Calculate recall and precision
+    accuracy = np.diag(confusion_matrix).sum() / confusion_matrix.sum()
+    print(confusion_matrix)
+    print("Accuracy " + str(100 * accuracy))
+    print(np.sum(confusion_matrix))
 
 
 if __name__ == "__main__":
